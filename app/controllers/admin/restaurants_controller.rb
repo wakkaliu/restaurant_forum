0653d21745d02f10c5c06d149c721fdc2db32dd9
Wakka Liu <wakkaliu@uniform.com.tw>
2018-07-04 01:55:59 -0700

    add restaurant edit and update cation ande views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -20,15 +20,32 @@ class Admin::RestaurantsController < ApplicationController
 		end
 	end
 
+	before_action :set_restaurant, only: [:show, :edit]
+
 	def show
-		@restaurant = Restaurant.find(params[:id])
 	end
 	
-	
+	def edit
+	end
+
+	def update
+		if @restaurant.update(restaurant_params)
+			flash[:notice] = "restaurant was successfully updated"
+			redirect_to admin_restaurant_path(@restaurant)
+		else
+			flash.now[:alert] = "restaurant was failed to update"
+			render :edit
+		end
+	end
+
 	private
 
 	def restaurant_params
 		params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+	end
+	
+	def set_restaurant
+		@restaurant = Restaurant.find(params[:id])
 	end
 	
 end
